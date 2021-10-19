130e2cbf88d306bc25ba6fed40a0b862775e21ea
Zak Ali <zak.ali@thetin.local>
2021-10-20 00:55:46 +0100

    Add debugging and add 'equals' to query

--- mutations/addToCart.ts
+++ mutations/addToCart.ts
@@ -7,18 +7,20 @@ async function addToCart(
   root: any,
   { productId }: { productId: string },
   context: KeystoneContext
-) {
+): Promise<any> {
   const user = context.session as Session;
-  if (!user.itemId) {
+  if (!user.itemId) { 
     throw new Error('You must be signed in!');
   }
 
+  console.log("Adding cart....")
   const allCartItems = await context.query.CartItem.findMany({
-    where: { user: { id: user.itemId }, product: { id: productId } },
+    where: { user: { id: { equals: user.itemId } }, product: { id: productId } },
     resolveFields: 'id,quantity,product'
   });
 
   const [existingCartItem] = allCartItems;
+  console.log(existingCartItem)
 
   if (existingCartItem) {
     return await context.db.CartItem.updateOne({
